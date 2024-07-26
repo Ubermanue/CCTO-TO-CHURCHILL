d6d9c14083e3080163de48efcbd997c0d133e78b
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-07-26 13:31:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ app.post('/login', async (req, res) => {
 		});
 	}
 });
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
