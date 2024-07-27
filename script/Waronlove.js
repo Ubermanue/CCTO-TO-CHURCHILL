6111af6545b61ba349e469928fb30e9e0a4517eb
Ubermanue <rinz135orugejehe@gmail.com>
2024-07-27 22:02:43 +0800

    Update and rename love.js to Waronlove.js

--- script/Waronlove.js
+++ script/Waronlove.js
@@ -2,17 +2,17 @@ const fs = require("fs-extra");
 const axios = require("axios");
 
 module.exports.config = {
-	name: "love",
+	name: "war",
 	version: "1.0.0",
 	role: 0,
-	credits: "GORDON",
-	description: "love in the chat box",
+	credits: "cliff",
+	description: "War in the chat box",
 	hasPrefix: false,
-	usages: "love start",
-	cooldown: 5,
+	usages: "war start",
+	cooldown: 10,
 };
 
-let isloveModeActive = false;
+let isWarModeActive = false;
 let messageTimeouts = [];
 let autoDeactivateTimeout;
 
@@ -21,9 +21,9 @@ function clearMessageTimeouts() {
 	messageTimeouts = [];
 }
 
-function startlove(api, event) {
+function startWar(api, event) {
 	const messages = [
-		"Bat love?",
+                "Bat love?",
 		"i love youuuuuuuuuuu",
 		"my honey bunche sugar plum ",
 		"Cute ng baby ko pakiss nga",
@@ -58,49 +58,49 @@ function startlove(api, event) {
 	
 	messages.forEach((msg, index) => {
 		const timeout = setTimeout(() => {
-			if (isloveModeActive) {
+			if (isWarModeActive) {
 				api.sendMessage(msg, event.threadID);
 			}
 		}, index * 5000);
 		messageTimeouts.push(timeout);
 	});
 
-	// Automatically deactivate love mode after 5 minutes (300000 milliseconds)
+	// Automatically deactivate war mode after 5 minutes (300000 milliseconds)
 	autoDeactivateTimeout = setTimeout(() => {
-		if (isloveModeActive) {
-			isloveModeActive = false;
+		if (isWarModeActive) {
+			isWarModeActive = false;
 			clearMessageTimeouts();
-			api.sendMessage("love mode automatically deactivated after 5 minutes.", event.threadID);
+			api.sendMessage("War mode automatically deactivated after 5 minutes.", event.threadID);
 		}
 	}, 300000);
 }
 
 module.exports.run = async function({ api, args, event, admin }) {
 	if (args.length === 0) {
-		api.sendMessage("Type 'love on' to activate war mode or 'love off' to deactivate love mode.", event.threadID);
+		api.sendMessage("Type 'war on' to activate war mode or 'war off' to deactivate war mode.", event.threadID);
 		return;
 	}
 	
 	const command = args[0].toLowerCase();
 	
 	if (command === "on") {
-		if (isloveModeActive) {
-			api.sendMessage("love is already active!", event.threadID);
+		if (isWarModeActive) {
+			api.sendMessage("War is already active!", event.threadID);
 		} else {
-			isloveModeActive = true;
-			api.sendMessage("love mode activated!", event.threadID);
+			isWarModeActive = true;
+			api.sendMessage("War mode activated!", event.threadID);
 			startWar(api, event);
 		}
 	} else if (command === "off") {
-		if (isloveModeActive) {
-			isloveModeActive = false;
+		if (isWarModeActive) {
+			isWarModeActive = false;
 			clearTimeout(autoDeactivateTimeout);
 			clearMessageTimeouts();
-			api.sendMessage("love mode deactivated!", event.threadID);
+			api.sendMessage("War mode deactivated!", event.threadID);
 		} else {
-			api.sendMessage("love mode is not active!", event.threadID);
+			api.sendMessage("War mode is not active!", event.threadID);
 		}
 	} else {
-		api.sendMessage("Invalid command! Use 'love on' to start and 'love off' to stop.", event.threadID);
+		api.sendMessage("Invalid command! Use 'war on' to start and 'war off' to stop.", event.threadID);
 	}
 };
